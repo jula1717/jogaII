1119490a842ebce47cd49e08f624753106145661
jula1717 <82888111+jula1717@users.noreply.github.com>
2024-03-20 09:57:46 +0100

    feat: change completed state from details

--- app/src/main/java/com/example/jogaii/ui/details/DetailsFragment.kt
+++ app/src/main/java/com/example/jogaii/ui/details/DetailsFragment.kt
@@ -6,6 +6,7 @@ import android.view.animation.AnimationUtils
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.lifecycle.lifecycleScope
 import com.example.jogaii.R
 import com.example.jogaii.animations.ProgressBarAnimation
 import com.example.jogaii.databinding.FragmentDetailsBinding
@@ -19,7 +20,6 @@ class DetailsFragment : Fragment(R.layout.fragment_details) {
         super.onViewCreated(view, savedInstanceState)
         val binding = FragmentDetailsBinding.bind(view)
         binding.apply {
-            //val asana = args.asana
             txtSanskritName.text = viewModel.sanskritName
             txtAsanaType.text = viewModel.type
             val anim = ProgressBarAnimation(pbDifficultyLevel, 0f, viewModel.difficulty * 1000f)
@@ -27,8 +27,8 @@ class DetailsFragment : Fragment(R.layout.fragment_details) {
             pbDifficultyLevel.setMax(5 * 1000)
             pbDifficultyLevel.startAnimation(anim)
             pbDifficultyLevel.setProgress(viewModel.difficulty * 1000)
-            val visibility = if (viewModel.completed) View.VISIBLE else View.INVISIBLE
-            imgDone.visibility = visibility
+            val color = if(viewModel.completed) R.color.s3 else R.color.light_gray
+            imgDone.setColorFilter(color)
             txtDescription.visibility = View.GONE
             txtDescription.text = viewModel.description
             imgAsana.setImageResource(viewModel.imgRes)
@@ -38,16 +38,30 @@ class DetailsFragment : Fragment(R.layout.fragment_details) {
                 viewModel.toggleImageVisibility()
             }
 
+            imgDone.setOnClickListener {
+                viewModel.completed = !viewModel.completed
+                viewModel.imgDoneClicked()
+            }
+
 
             viewModel.imgVisible.observe(viewLifecycleOwner, Observer { imgVisible ->
 
-                if(imgVisible){
+                if (imgVisible) {
                     imgAsana.startAnimation(AnimationUtils.loadAnimation(context, R.anim.zoom_in))
-                    txtDescription.startAnimation(AnimationUtils.loadAnimation(context, R.anim.zoom_out))
-                }
-                else{
+                    txtDescription.startAnimation(
+                        AnimationUtils.loadAnimation(
+                            context,
+                            R.anim.zoom_out
+                        )
+                    )
+                } else {
                     imgAsana.startAnimation(AnimationUtils.loadAnimation(context, R.anim.zoom_out))
-                    txtDescription.startAnimation(AnimationUtils.loadAnimation(context, R.anim.zoom_in))
+                    txtDescription.startAnimation(
+                        AnimationUtils.loadAnimation(
+                            context,
+                            R.anim.zoom_in
+                        )
+                    )
                 }
 
                 imgAsana.visibility = if (imgVisible) View.VISIBLE else View.GONE
@@ -56,5 +70,20 @@ class DetailsFragment : Fragment(R.layout.fragment_details) {
             })
 
         }
+
+        viewLifecycleOwner.lifecycleScope.launchWhenStarted {
+            viewModel.detailsEvent.collect { event ->
+                when (event) {
+                    is DetailsViewModel.DetailsEvent.changeImgDone -> {
+                        val color = if(event.completed) R.color.s3 else R.color.light_gray
+                        binding.apply {
+                            imgDone.setColorFilter(color)
+                        }
+                    }
+                }
+            }
+        }
     }
+
+}
 
--- app/src/main/java/com/example/jogaii/ui/details/DetailsViewModel.kt
+++ app/src/main/java/com/example/jogaii/ui/details/DetailsViewModel.kt
@@ -3,9 +3,14 @@ package com.example.jogaii.ui.details
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.jogaii.data.Asana
 import com.example.jogaii.data.AsanaDao
 import com.example.jogaii.data.AsanaWithType
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -16,52 +21,75 @@ class DetailsViewModel @Inject constructor(
 
     var imgVisible = MutableLiveData<Boolean>().apply { value = true }
 
+    private val detailsEventChannel = Channel<DetailsEvent>()
+    val detailsEvent = detailsEventChannel.receiveAsFlow()
+
     val asana = state.get<AsanaWithType>("asana")
 
     var asanaName = state.get<String>("asanaName") ?: asana?.asana?.name ?: ""
-        set(value){
+        set(value) {
             field = value
-            state.set("asanaName",value)
+            state.set("asanaName", value)
         }
 
     var sanskritName = state.get<String>("sanskritName") ?: asana?.asana?.sanskritName ?: ""
-        set(value){
+        set(value) {
             field = value
-            state.set("sanskritName",value)
+            state.set("sanskritName", value)
         }
 
     var description = state.get<String>("description") ?: asana?.asana?.description ?: ""
-        set(value){
+        set(value) {
             field = value
-            state.set("description",value)
+            state.set("description", value)
         }
 
     var difficulty = state.get<Int>("difficulty") ?: asana?.asana?.difficulty ?: 0
-        set(value){
+        set(value) {
             field = value
-            state.set("difficulty",value)
+            state.set("difficulty", value)
         }
 
     var imgRes = state.get<Int>("imgRes") ?: asana?.asana?.imgRes ?: 0
-        set(value){
+        set(value) {
             field = value
-            state.set("imgRes",value)
+            state.set("imgRes", value)
         }
 
     var completed = state.get<Boolean>("completed") ?: asana?.asana?.completed ?: false
-        set(value){
+        set(value) {
             field = value
-            state.set("completed",value)
+            state.set("completed", value)
         }
 
     var type = state.get<String>("type") ?: asana?.type?.typeName ?: ""
-        set(value){
+        set(value) {
             field = value
-            state.set("type",value)
+            state.set("type", value)
         }
 
     fun toggleImageVisibility() {
         imgVisible.value = !(imgVisible.value ?: true)
+    }
+
+    fun imgDoneClicked() {
+        val updatedAsana: Asana? = asana?.asana?.copy(completed = completed)
+        updatedAsana?.let {
+            updateAsana(it)
+            changeImgDone(completed)
+        }
+    }
+
+    private fun updateAsana(updatedAsana: Asana) = viewModelScope.launch {
+        asanaDao.update(updatedAsana)
+    }
+
+    private fun changeImgDone(completed: Boolean) = viewModelScope.launch {
+        detailsEventChannel.send(DetailsEvent.changeImgDone(completed))
+    }
+
+    sealed class DetailsEvent {
+        data class changeImgDone(val completed: Boolean) : DetailsEvent()
     }
 
 }
